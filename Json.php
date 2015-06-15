a660f695b975a0b03e7750ea36e3e693a9432c61
RGT1 <rgviro@gmail.com>
2015-06-15 20:45:12 +0200

    Json Changes
    Json get and set modified and adapted to requisites

--- Json.php
+++ Json.php
@@ -5,30 +5,49 @@ $user = "root";
 $pass = "";
 $bd = "icsypbdb";
  
+
+//http://ejemplocodigo.com/ejemplo-php-crear-y-leer-json-de-una-tabla-mysql/
+
 //Creamos la conexión
 $conexion = mysqli_connect($server, $user, $pass,$bd) 
 or die("Ha sucedido un error inexperado en la conexion de la base de datos");
- 
-//generamos la consulta
+
+//generamos las consultas
 $sql = "SELECT DESCRIPCION,IDRUTA  FROM ruta";
 mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
  
 if(!$result = mysqli_query($conexion, $sql)) die();
  
-$rutas = array(); //creamos un array
- 
+$rutas = array(); //creamos un array 
+
 while($row = mysqli_fetch_array($result)) 
 { 
     $nombre=$row['DESCRIPCION']; 
     $id=$row['IDRUTA']; 
-    $rutas[] = array('id'=> $id, 'descripcion'=> $nombre);
+   
  //enlazamos las balizas
-
-
-
-}
-    
-//desconectamos la base de datos
+	//generamos la consulta anidada
+	$sql2 = "SELECT `balizasjson`.`IDBALIZA`,`balizasjson`.`TEXTO_ID`, ,`balizasjson`.`MAC` ,`balizasjson`.`POSICION` ,`balizasjson`.`IDUSUARIO` ,`balizasjson`.`ESTROPEADO` ,`balizasjson`.`EMAIL` FROM `balizasjson` WHERE `balizasjson`.`IDRUTA`= " & $id & " ORDER BY `balizasjson`.`IDBALIZA`)";
+	mysqli_set_charset($conexion2, "utf8"); //formato de datos utf8
+	 
+	if(!$result2 = mysqli_query($conexion, $sql2)) die();
+	 
+	$balizas = array(); //creamos un array
+	 
+	while($row = mysqli_fetch_array($result2)) 
+	{ 	    
+		$idb=$row['IDBALIZA'];
+        $texto_idb=$row['TEXTO_ID'];
+        $mac=$row['MAC'];
+        $posicion=$row['POSICION'];
+        $id_contacto=$row['IDUSUARIO'];
+        $estropeado=$row['ESTROPEADO'];
+        $mail=$row['MAIL'];
+        $balizas[] = array('idb'=> $idb, 'descripcion'=> $texto_idb,'mac'=> $mac,'posicion'=> $posicion,'idcontacto'=>$id_contacto,'estropeado'=>$estropeado,'email'=>$mail);
+	}
+	   $rutas[] = array('id'=> $id, 'descripcion'=> $nombre,'balizas'=> $balizas);
+	}  
+	//desconectamos la base de datos
 $close = mysqli_close($conexion) 
 or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
   
@@ -37,11 +56,7 @@ or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
 $json_string = json_encode($rutas, JSON_NUMERIC_CHECK);
 echo $json_string;
  
-//Si queremos crear un archivo json, sería de esta forma:
-/*
-$file = 'clientes.json';
-file_put_contents($file, $json_string);
-*/
+/
     
  
 ?>
