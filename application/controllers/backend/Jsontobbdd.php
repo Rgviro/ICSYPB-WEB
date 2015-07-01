296fc828e4e147d45cff02ad543d58201f51bb28
root <root@PandaBoard.localdomain>
2015-07-01 05:50:55 +0200

    Cambios en Jsontobbdd para parseo de Json

--- application/controllers/backend/Jsontobbdd.php
+++ application/controllers/backend/Jsontobbdd.php
@@ -2,59 +2,37 @@
  
 class jsontobbdd extends CI_Controller {
 
-	public function give_me_post_params()
-  {
-    $result = $_POST;
-    // Check if Content Type is JSON
-    if( isset( $_SERVER['CONTENT_TYPE'] ) &&
-        strpos( $_SERVER['CONTENT_TYPE'], "application/json" ) !== false )
-    {      
-      $jsonData = json_decode( trim( file_get_contents( 'php://input' ) ), true );  
-      $result = $jsonData;
-    }
-    return $result;
-  }
-
-    public function index()
-	    {
-	    	die(var_dump($_POST));
-		$server = "ctcloud.sytes.net";
-		$user = "root";
-		$pass = "";
-		$bd = "icsypbdb";
-		$this->output->enable_profiler(TRUE);
-		$postData = $this->input->post();
+    public function index() {
+	$server = "localhost";
+	$user = "ICSYPB";
+	$pass = "ICSYPB";
+	$bd = "icsypbdb";
 		
 
+        $rawBody = file_get_contents("php://input");
+        $data = json_decode($rawBody, true);
 
-
-
-		$this->give_me_post_params();/*
+        header('Content-Type: application/json; charset=UTF-8');
+        //echo json_encode(array('data' => $data));
 
 		//Creamos la conexión
 		$conexion = mysqli_connect($server, $user, $pass,$bd) 
 		or die("Ha sucedido un error inexperado en la conexion de la base de datos");
 
-		if($_POST){
-			/**leer el fichero recibido y convertir su contenido*/
+		if($data){
 			//$str_datos = file_get_contentes("tracking.json");
 			//$datos = json_decode($str_datos,true);
 			/*Leer datos del POSt*/
-			/*$misdatosjson =json_decode($_POST["datos"]);
-			foreach ($misdatosjson as $dato ) {
-				echo $dato[0];
+			foreach ($data as $dato ) {
+				//echo $dato[1];
+				//print_r($dato);
 				//generamos la consulta
-				//$sql = "INSERT INTO tracking (MAC_USUSARIO,ID_RUTA,ID_BALIZA,MAC_BALIZA,DESC_BALIZA,POSICION,FECHA,IDTRACKPUB) values($datos[0],$datos[1],$datos[2],$datos[3],$datos[4],$datos[5],$datos[6],$datos[7]) ";
-				$sql = "INSERT INTO tracking (MAC_USUSARIO,ID_RUTA,ID_BALIZA,MAC_BALIZA,DESC_BALIZA,POSICION,FECHA,IDTRACKPUB) values($dato[0],$dato[1],$dato[2],$dato[3],$dato[4],$dato[5],$dato[6],$dato[7]) ";
+				$sql = "INSERT INTO tracking (MAC_USUSARIO,ID_RUTA,ID_BALIZA,MAC_BALIZA,DESC_BALIZA,POSICION,FECHA,IDTRACKPUB) values('" .$dato["mac_usuario"]. "'," .$dato["id_ruta"]. "," .$dato["id_baliza"]. ",'" .$dato["mac_baliza"]. "','" .$dato["desc_baliza"]. "','" .$dato["posicion"]. "','". $dato["fecha"]. "')";
 				mysqli_set_charset($conexion, "utf8"); //formato de datos utf8
+			 	// DEBUG print_r($sql);	
 				$result = mysqli_query($conexion, $sql);
-
-				# code...
 			}
 		}
-	else{
-	//echo "Datos no recibidos";
-	}*/
 }
 
 }
